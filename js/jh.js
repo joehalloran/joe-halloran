40805ad8609ead9998f6fb2d97ea1053d6567445
joehalloran <joestandard@Mac-Pro-Joe.local>
2017-01-29 17:26:41 +0000

    creation of project pages, images, and css

--- js/jh.js
+++ js/jh.js
@@ -1,16 +1,52 @@
 $(function () {
 	
-	// $topLevelElements = $('nav, main, footer');
-	// $topLevelElements.hide();
 
-	// $('body').width(0).animate({"width":"100%"}, 1000, 'swing', showElements);
-	// typing();
+	$topLevelElements = $('header, main, footer');
+	$heightCache = $(document).height();
+	$topLevelElements.hide();
+
+	$('#pre-loader').height($heightCache);
+	$('#pre-loader').width(0).animate({"width":"100%"}, 1000, 'swing', showElements);
+	typing();
 
 	$('input, textarea').on('keyup click', moveUpFormGroup);
 
+	$('.js-fade-in').css({"opacity":0});
+	$('.js-slide-down').hide();
+	$('.js-slide-up').hide();
+
+	$(window).on('scroll', function() {
+
+		if ( $(window).width() > 992 ) {
+			if ( $('.js-fade-in').offset().top  < ( $(window).scrollTop() + ($(window).height() / 2) ) ) {
+				$('.js-fade-in').animate({"opacity":1}, 3000 );	
+			}
+
+			if ( $('.js-slide-down').parent().offset().top < ( $(window).scrollTop() + ($(window).height() / 4) ) ) {
+				$('.js-slide-down').show('slide', {direction: 'right'}, 1000);
+			}
+
+			if ( $('.js-slide-up').parent().offset().top < ( $(window).scrollTop() + ($(window).height() * 0.75) ) ) {
+				$('.js-slide-up').show('slide', {direction: 'down'}, 1000);
+			}
+		} else {
+			$('.js-fade-in').css({"opacity":1});
+			$('.js-slide-down').show();
+			$('.js-slide-up').show();
+		}
+	});
+
+	$('.project-btn').click( function( event ) {
+		event.preventDefault();
+		$('html, body').animate({
+	        scrollTop: $(".project-intro").offset().top
+	    }, 1000);
+	});
+
 
 	function showElements() {
-		$topLevelElements.fadeIn(1000);
+		$('#pre-loader').height('0px');
+		$topLevelElements.fadeIn(2000);
 		typing()
 	}
 
